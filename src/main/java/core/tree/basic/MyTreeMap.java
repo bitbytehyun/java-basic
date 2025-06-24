4b5a783adf08afa03d1558b3c50b44ac46cdfc1d
seohyun jeong <jeongseohyun31@gmail.com>
2025-06-24 23:18:37 +0900

    add: TreeMap 구현 추가

---
+++ src/main/java/core/tree/basic/MyTreeMap.java
@@ -0,0 +1,49 @@
+package core.tree.basic;
+
+public class MyTreeMap<K extends Comparable<K>, V> {
+    private static class Node<K, V> {
+        K key;
+        V value;
+        Node<K, V> left;
+        Node<K, V> right;
+
+        Node(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    private Node<K, V> root;
+
+    public V get(K key) {
+        Node<K, V> node = root;
+        while (node != null) {
+            int cmp = key.compareTo(node.key);
+            if (cmp < 0) {
+                node = node.left;
+            } else if (cmp > 0) {
+                node = node.right;
+            } else {
+                return node.value;
+            }
+        }
+        return null;
+    }
+
+    public void put(K key, V value) {
+        root = put(root, key, value);
+    }
+
+    private Node<K, V> put(Node<K, V> node, K key, V value) {
+        if (node == null) return new Node<>(key, value);
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0) {
+            node.left = put(node.left, key, value);
+        } else if (cmp > 0) {
+            node.right = put(node.right, key, value);
+        } else {
+            node.value = value;
+        }
+        return node;
+    }
+}
